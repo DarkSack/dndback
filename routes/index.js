c41423f947b874383c13ff7ff0ad8678ce0d89db
DarkSack <johanjafet4@gmail.com>
2025-08-25 21:11:27 -0600

    try3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.json({
+    message: "D&D Backend API",
+    title: "Express",
+    status: "running",
+    endpoints: [
+      "/users",
+      "/auth/login",
+      "/auth/register",
+      "/characters/create",
+      "/characters/summon",
+      "/histories/create",
+      "/histories/continue",
+      "/room/create",
+      "/extras/improveField",
+    ],
+  });
 });
 
 export default router;
